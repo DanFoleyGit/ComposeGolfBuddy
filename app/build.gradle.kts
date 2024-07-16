d7c50df2eeda4702797fb9e59aef22e8b3f63cf7
DanFoleyGit <danfoley1122@gmail.com>
2024-07-16 13:15:55 +1000

    Updated Gradle and added unit testing libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    id("de.mannodermaus.android-junit5") version "1.9.3.0"
 }
 
 android {
@@ -12,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.composegolfbuddy"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -84,24 +85,30 @@ dependencies {
     // Optional - Add full set of material icons
     implementation ("androidx.compose.material:material-icons-extended")
 
-    // LiveData
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
-//    // ViewModel
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-runtime-ktx:2.5.1")
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
-//    implementation ("androidx.fragment:fragment-ktx:1.6.2")
-//    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-
     // Room
     implementation ("androidx.room:room-ktx:2.5.2")
+    implementation ("androidx.room:room-runtime:2.5.2")
     kapt ("androidx.room:room-compiler:2.5.2")
-    implementation ("androidx.room:room-runtime:2.5.1")
-    annotationProcessor ("androidx.room:room-compiler:2.5.1")
 
     // Hilt
     implementation ("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    // testing
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+
+    testImplementation("com.willowtreeapps.assertk:assertk:0.26.1")
+    testImplementation("io.mockk:mockk:1.12.5")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("app.cash.turbine:turbine:0.7.0")
+
+    androidTestImplementation("io.mockk:mockk-android:1.12.5")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    androidTestImplementation("app.cash.turbine:turbine:0.7.0")
+
 }
 
 // Allow references to generated code

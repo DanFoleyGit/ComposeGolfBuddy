4a4dbccb8f4dc190ccdaf45373e0ea37fd0107aa
DanFoleyGit <danfoley1122@gmail.com>
2024-07-16 14:16:55 +1000

    Fixed bug to display clubs in descending order

--- app/src/main/java/com/example/composegolfbuddy/model/ClubTypes.kt
+++ app/src/main/java/com/example/composegolfbuddy/model/ClubTypes.kt
@@ -3,8 +3,10 @@ package com.multiplatform.clubdistances.homeScreen.model
 class ClubTypes() {
     fun getClubTypes(): List<String> {
         return listOf(
-            "D", "3W", "5W", "7W", "3i", "4i", "5i", "6i", "7i",
-            "8i", "9i", "PW", "GW", "SW", "LW"
+            "D", "3W", "5W", "7W",
+            "3H", "4H", "5H",
+            "2i", "3i", "4i", "5i", "6i", "7i", "8i", "9i",
+            "PW", "GW", "SW", "LW"
         )
     }
 }
